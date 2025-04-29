362ec0feaf314983a8e3f5270317f0c65518428f
IgnacyBerent <2gb02ignac@gmail.com>
2025-04-29 11:35:00 +0200

    responsivity adjustment

--- src/features/project/components/column-title.tsx
+++ src/features/project/components/column-title.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 const ColumnTitle = (props: Props) => {
   return (
-    <th className="px-6 py-3 text-left text-base font-medium text-white/40 border-b border-white/10">
+    <th className="px-3 py-3 text-left text-base font-medium text-white/40 border-b border-white/10">
       <div className="flex gap-2 items-center">
         {props.icon}
         {props.title}
